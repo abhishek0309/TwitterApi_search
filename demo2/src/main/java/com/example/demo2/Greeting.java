93ed863a12125c746961d5bb585784f862636922
abhishek0309 <43817970+abhishek0309@users.noreply.github.com>
2020-10-08 20:13:37 +0530

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo2/Greeting.java
@@ -0,0 +1,37 @@
+package com.example.demo2;
+
+public class Greeting {
+
+    private String followers;
+    private String keywords;
+    private String twitter;
+
+
+    public String getFollowers() {
+
+        return followers;
+    }
+    public String getKeywords() {
+
+        return keywords;
+    }
+
+    public String getTwitter(){
+        return twitter;
+    }
+
+    public void setFollowers(String followers) {
+
+        this.followers = followers;
+    }
+    public void setKeywords(String keywords) {
+
+        this.keywords = keywords;
+    }
+
+    public void setTwitter(String twitter){
+        this.twitter=twitter;
+    }
+
+
+}
---
+++ demo2/src/main/java/com/example/demo2/GreetingController.java
@@ -0,0 +1,88 @@
+package com.example.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        // Run this on Windows, cmd, /c = terminate after this run
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        StringBuffer sb=new StringBuffer("curl -X GET -H \"Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAAHGIAEAAAAA0Fh2YyDpV1N336rFJaFFW8FOzfA%3DoRjcBWlUguDD6pdPOtRl39v4ELBSOJ6EesR8FeyHrAIrE06wwV\" ");
+        if (greeting.getKeywords()==null) {
+            sb.append("https://api.twitter.com/1.1/search/tweets.json?q=from%3A");
+            sb.append(greeting.getFollowers());
+            sb.append("&result_type=recent");
+        }
+        else if(greeting.getFollowers()==null){
+            sb.append("https://api.twitter.com/1.1/search/tweets.json?q=%23");
+            sb.append(greeting.getKeywords());
+            sb.append("&result_type=popular");
+
+        }
+        else{
+            sb.append("https://api.twitter.com/1.1/search/tweets.json?q=from%3A");
+            sb.append(greeting.getFollowers());
+            sb.append("%20");
+            sb.append(greeting.getKeywords());
+            sb.append("&result_type=popular");
+
+        }
+
+
+
+        processBuilder.command("cmd.exe", "/c",sb.toString());
+        try {
+            StringBuffer sb2=new StringBuffer("DataBase");
+            sb2.append(greeting.getFollowers());
+            sb2.append(greeting.getKeywords());
+            sb2.append(".json");
+            FileWriter myWriter = new FileWriter(sb2.toString());
+
+            Process process = processBuilder.start();
+
+            // blocked :(
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            StringBuffer dev=new StringBuffer();
+
+            while ((line = reader.readLine()) != null) {
+                dev.append(line);
+                myWriter.write(line);
+            }
+            greeting.setTwitter(dev.toString());
+            myWriter.close();
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "result";
+    }
+
+}
